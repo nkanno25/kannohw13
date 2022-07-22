36c1e7b1d36448b208c4383cf03162559fadd389
Luka <nickolas.kanno@gmail.com>
2022-07-22 14:55:14 -0700

    final edit

--- Develop/seeds/product-seeds.js
+++ Develop/seeds/product-seeds.js
@@ -2,31 +2,31 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Shirt',
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'Running Shoes',
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: 'Baseball Hat',
+    product_name: 'Hat',
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: 'Music Compilation Record',
+    product_name: 'Music Records',
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    product_name: 'Basketball Shorts',
     price: 29.99,
     stock: 22,
     category_id: 2,
